6600da60f1e7e134fe34e2e6ec2026b41e0e3081
sdeix <sdeix2765@gmail.com>
2023-09-28 21:51:05 +0700

    part8

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -74,3 +74,18 @@ class AuthorDetailView(generic.DetailView):
         context = super().get_context_data(*args, **kwargs)
         return context
     template_name = 'author_detail.html'
+
+
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """
+    Generic class-based view listing books on loan to current user.
+    """
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -13,7 +13,9 @@ from django.views.generic import RedirectView
 urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
-
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
 from django.conf import settings
 from django.conf.urls.static import static
 
