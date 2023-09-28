6712586e0745d9b42540518d90166c7059cab1be
sdeix <sdeix2765@gmail.com>
2023-09-28 20:23:30 +0700

    part6

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
-
+from django.urls import re_path as url
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -22,3 +22,48 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'number_wild_books':number_wild_books,'number_wild_genres':number_wild_genres},
     )
+
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+    def get_context_data(self, **kwargs):
+            context = super(BookListView, self).get_context_data(**kwargs)
+
+            context['some_data'] = 'This is just some data'
+            return context
+    template_name = 'book_list.html'  # Определение имени вашего шаблона и его расположения
+    
+class BookDetailView(generic.DetailView):
+
+    model = Book
+
+    def get_context_data(self, **kwargs):
+            context = super(BookDetailView, self).get_context_data(**kwargs)
+
+            context['some_data'] = 'This is just some data'
+            return context
+    template_name = 'book_detail.html'
+
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+    def get_context_data(self, **kwargs):
+            context = super(AuthorListView, self).get_context_data(**kwargs)
+
+            context['some_data'] = 'This is just some data'
+            return context
+    template_name = 'author_list.html'  
+    
+class AuthorDetailView(generic.DetailView):
+
+    queryset = Author.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        return context
+    template_name = 'author_detail.html'
